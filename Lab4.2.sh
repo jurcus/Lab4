6f43e8ae91d2e745207478a9ec97de5fd43f73f1
DSW51405 <jurecki03@wp.pl>
2023-04-14 01:23:21 +0200

    Myślnik drugi cz. 2

---
+++ Lab4.2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ "$1" = "--date" || "$1" = "-d" ]; then
+  echo "Dzisiejsza data: $(date +%Y-%m-%d)"
+elif [ "$1" = "--logs" || "$1" = "-l" ]; then
+  if [ -z "$2" ]; then
+    echo "Użycie: $0 --logs <liczba plików>"
+  else
+    num_logs=$2
+    for i in $(seq 1 $num_logs); do
+      filename="log$i.txt"
+      echo "Nazwa pliku: $filename" > $filename
+      echo "Nazwa skryptu: $0" >> $filename
+      echo "Data: $(date +%Y-%m-%d)" >> $filename
+    done
+    echo "Utworzono $num_logs plików logx.txt"
+  fi
+elif [ "$1" = "--help" || "$1" = "-h" ]; then
+  echo "Użycie: $0 --date || --logs <liczba plików> || --help"
+  echo "  --date lub -d                                 Wyświetla dzisiejszą datę."
+  echo "  --logs <liczba plików> lub -l <liczba plików> Tworzy określoną liczbę plików logx.txt."
+  echo "  --help lub -h                                 Wyświetla krótką pomoc."
+else
+  echo "Użycie: $0 --date || --logs <liczba plików> || --help"
+fi

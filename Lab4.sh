ffb597970ad8e14ca073979d0717bafda4946da5
DSW51405 <jurecki03@wp.pl>
2023-04-14 01:13:12 +0200

    Czwarty myślnik

--- Lab4.sh
+++ Lab4.sh
@@ -1,25 +1,20 @@
-#1/bin/bash
+#!/bin/bash
 
 if [ "$1" = "--date" ]; then
-echo "Dzisiejsza data: $)date +%Y-%m-%d)"
+  echo "Dzisiejsza data: $(date +%Y-%m-%d)"
 elif [ "$1" = "--logs" ]; then
-if [ -z "$2" ]; then
-echo "Użycie: $0 --logs <liczba plików>"
+  if [ -z "$2" ]; then
+    echo "Użycie: $0 --logs <liczba plików>"
+  else
+    num_logs=$2
+    for i in $(seq 1 $num_logs); do
+      filename="log$i.txt"
+      echo "Nazwa pliku: $filename" > $filename
+      echo "Nazwa skryptu: $0" >> $filename
+      echo "Data: $(date +%Y-%m-%d)" >> $filename
+    done
+    echo "Utworzono $num_logs plików logx.txt"
+  fi
 else
-num_logs=$2
-for i in $(seq 1 $num_logs); do
-filename="log$i.txt"
-echo "Nazwa pliku: $filename" > $filename
-echo "Nazwa skryptu: $0" >> $filename
-echo "Data: $(date +%Y-%m-%d)" >> $filename
-done
-echo "Utworzono $num_logs plików logx.txt"
-fi
-elif [ "$1" = "--help" ]; then
-echo "UŻycie: $0 --date | --logs <liczba plików> | --help"
-echo "  --date     Wyświetla dzisiejszą datę"
-echo "  --logs <liczba plików> Tworzy określoną liczbę plików logx.txt"
-echo "  --help Wyświetla krótką pomoc"
-else
-echo "Użycie: $0 --date | --logs <liczba plików> | --help"
+  echo "Użycie: $0 --date | --logs <liczba plików>"
 fi
